a04ff738c5b41844ae3a802330ff417bb714be99
Gabriel Massensini <89725700+Massensini@users.noreply.github.com>
2022-11-29 12:55:20 -0300

    menu desk and menu mobile finished

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuMobile = document.getElementById('menu-mobile')
+const headerMobile = document.getElementById('header')
+
+menuMobile.addEventListener('click', () => {
+    headerMobile.classList.toggle('vissible') 
+})
+
