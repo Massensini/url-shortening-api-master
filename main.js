f1e59074b5608ef217bae61e2b27ed915c800b9e
Gabriel Massensini <89725700+Massensini@users.noreply.github.com>
2022-11-29 21:16:24 -0300

    css and html of inputs finished

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ const menuMobile = document.getElementById('menu-mobile')
 const headerMobile = document.getElementById('header')
 
 menuMobile.addEventListener('click', () => {
-    headerMobile.classList.toggle('vissible') 
+    headerMobile.classList.toggle('vissible')
 })
+
+function verifyUrl() {
+    let url = document.getElementById('url-text')
+    const errorMsg = document.getElementById('error-message')
+    const submitUrl = document.getElementById('submit-url')
+    let regexUrl = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    let regex = new RegExp(regexUrl)
+    
+
+    submitUrl.addEventListener('click', () => {
+        if(url.value.match(regex)) {
+            url.classList.remove('error')
+            errorMsg.style.display = 'none'
+            console.log('certo')
+        } else {
+            url.classList.add('error')
+            errorMsg.style.display = 'flex'
+            console.log('errado')
+        }
+    })
+}
+
+verifyUrl()
 
